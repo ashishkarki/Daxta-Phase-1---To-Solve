0d6903582129fa8c5bc14164a472cc18b9f73d2b
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-09-08 18:39:15 +0545

    Implemented logic to perform operation Successful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,56 @@
-import { Component } from "@angular/core";
-import { Observable, BehaviorSubject, of } from "rxjs";
-import { AppService, NestedArray } from "./app-service.service";
+import { flatten } from '@angular/compiler'
+import { Component } from '@angular/core'
+import { Observable, BehaviorSubject, of, combineLatest } from 'rxjs'
+import { AppService, NestedArray } from './app-service.service'
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.sass"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  NestedArray$: BehaviorSubject<any[]>;
-  ReductionOperator$: BehaviorSubject<string>;
-  Calculated$: Observable<number>;
+  NestedArray$: BehaviorSubject<any[]>
+  ReductionOperator$: BehaviorSubject<string>
+  Calculated$: Observable<number>
+
+  CalculatedSubject$: BehaviorSubject<number>
 
   constructor(private _svc: AppService) {
-    this.ReductionOperator$ = new BehaviorSubject<string>("Add");
-    this.NestedArray$ = new BehaviorSubject<any>(_svc.GetNestedArray());
+    this.ReductionOperator$ = new BehaviorSubject<string>('Add')
+    this.NestedArray$ = new BehaviorSubject<any>(_svc.GetNestedArray())
     //TODO: Define Calculated$ Here
-    this.Calculated$ = of(0);
+    this.CalculatedSubject$ = new BehaviorSubject<number>(0)
+    this.Calculated$ = this.CalculatedSubject$.asObservable()
+  }
+
+  ngOnInit() {
+    combineLatest(this.ReductionOperator$, this.NestedArray$).subscribe(
+      ([operator, arr]) => {
+        const flattenedArr = flatten(arr)
+
+        let result = 0
+        if (operator === 'Add') {
+          result = flattenedArr.reduce((a, b) => a + b, 0)
+        } else if (operator === 'Multiply') {
+          result = flattenedArr.reduce((a, b) => a * b, 1)
+        }
+
+        this.CalculatedSubject$.next(result)
+      }
+    )
   }
 
   emitNewArray(e) {
     //Do not alter
-    this.NestedArray$.next(this._svc.GetNestedArray());
+    this.NestedArray$.next(this._svc.GetNestedArray())
   }
   toggleOperator(e) {
     //Do not alter
-    let nextOperator: string = "";
-    if (this.ReductionOperator$.value === "Add") {
-      this.ReductionOperator$.next("Multiply");
+    let nextOperator: string = ''
+    if (this.ReductionOperator$.value === 'Add') {
+      this.ReductionOperator$.next('Multiply')
     } else {
-      this.ReductionOperator$.next("Add");
+      this.ReductionOperator$.next('Add')
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
